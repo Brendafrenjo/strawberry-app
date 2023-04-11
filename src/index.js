e2fd887557aeeb50dd51cea9454acd40dc5b3a63
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-04-11 15:23:33 +0300

    updated temperature elements

--- src/index.js
+++ src/index.js
@@ -21,9 +21,48 @@ function formatDate(time) {
   return `${day} ${hour}:${minutes}`;
 }
 
+function showTemperature(response) {
+  let tempElement = document.querySelector("#current-temperature");
+
+  celsiusTemperature = response.data.main.temp;
+
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+}
+
 let cityElement = document.querySelector(".city");
 cityElement.innerHTML = "Nairobi";
 
 let timeElement = document.querySelector("#current-time");
 let date = new Date();
 timeElement.innerHTML = formatDate(date);
+
+let apiKey = `c49ac0d4966a15e494a3ad92063a514f`;
+let apiEndpoint = `https://api.openweathermap.org/data/2.5/weather?`;
+let apiUrl = `${apiEndpoint}q=Nairobi&appid=${apiKey}&units=metric`;
+
+function showFahrenheitTemp(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
+  let currentTemp = document.querySelector("#current-temperature");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  currentTemp.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function showCelsiusTemp(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  let currentTemp = document.querySelector("#current-temperature");
+  currentTemp.innerHTML = Math.round(celsiusTemperature);
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsiusTemp);
+
+let celsiusTemperature = null;
+
+axios.get(apiUrl).then(showTemperature);
